3baf091316c47885cf55b5a41a5b6e530ce16207
Bernardthe3rd <bammeijer@outlook.com>
2024-03-16 13:48:28 +0100

    started assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import axios from "axios";
 
 function App() {
+
+    async function fetchPosts () {
+        try {
+            const response = await axios.get("http://localhost:3000/posts")
+            console.log(response)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
   return (
     <>
-      <h1>Programmeer hier jouw applicatie</h1>
+        <h1>Programmeer hier jouw applicatie</h1>
+        <button type={"button"} onClick={fetchPosts}>Get all posts</button>
     </>
   )
 }
